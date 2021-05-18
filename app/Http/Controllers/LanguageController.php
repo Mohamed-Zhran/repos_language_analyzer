b641a9895942732c1b8f32d103aa197dbc6da501
MohamedZhran <mohamedzhran1@gmail.com>
2021-05-18 09:43:46 +0200

    push repo to language function and getters

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -12,14 +12,42 @@ class LanguageController extends Controller
 
     public function __construct($name)
     {
-        $this->reposTotalNumber=0;
-        $this->repos=[];
-        $this->name=$name;
+        $this->reposTotalNumber = 0;
+        $this->repos = [];
+        $this->name = $name;
     }
 
-    public function increaseReposNumber() {
+    public function increaseReposNumber()
+    {
         $this->reposTotalNumber++;
     }
 
+    public function pushRepoUrlToLanguage($repoUrl)
+    {
+        $this->repos[] = $repoUrl;
+    }
 
+    /**
+     * Get the value of reposTotalNumber
+     */
+    public function getReposTotalNumber()
+    {
+        return $this->reposTotalNumber;
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the value of repos
+     */
+    public function getRepos()
+    {
+        return $this->repos;
+    }
 }
