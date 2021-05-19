970d958ac37dc255e5f8a516ff2cead06a93090e
MohamedZhran <mohamedzhran1@gmail.com>
2021-05-19 09:15:10 +0200

    function to get how many repos in language

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -4,50 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class LanguageController extends Controller
-{
-    private $reposTotalNumber;
+class LanguageController extends Controller {
     private $repos;
     private $name;
 
-    public function __construct($name)
-    {
-        $this->reposTotalNumber = 0;
+    public function __construct($name) {
         $this->repos = [];
         $this->name = $name;
     }
 
-    public function increaseReposNumber()
-    {
-        $this->reposTotalNumber++;
-    }
-
-    public function pushRepoUrlToLanguage($repoUrl)
-    {
+    public function pushRepoUrlToLanguage($repoUrl) {
         $this->repos[] = $repoUrl;
-    }
-
-    /**
-     * Get the value of reposTotalNumber
-     */
-    public function getReposTotalNumber()
-    {
-        return $this->reposTotalNumber;
     }
 
     /**
      * Get the value of name
      */
-    public function getName()
-    {
+    public function getName() {
         return $this->name;
     }
 
     /**
      * Get the value of repos
      */
-    public function getRepos()
-    {
+    public function getRepos() {
         return $this->repos;
     }
+
+    public function getReposCount() {
+        return count($this->repos);
+    }
+
 }
