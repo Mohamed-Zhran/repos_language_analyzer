459e74f3f244c48faa19cca7179993d9f58cc765
MohamedZhran <mohamedzhran1@gmail.com>
2021-05-19 09:19:58 +0200

    push language object with language as key

--- app/Http/Controllers/ReposProcessor.php
+++ app/Http/Controllers/ReposProcessor.php
@@ -3,26 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Http;
+use App\Http\Controllers\LanguageController;
 
-class ReposProcessor extends Controller
-{
+class ReposProcessor extends Controller {
     private $apiUrl;
-    private $allLanguages;
-    private $allLanguagesUrls;
-    private $dateSinceAMonth;
+    private $languages;
+    private $reposLanguagesUrls;
+    private $dateSinceMonth;
 
-    public function __construct()
-    {
-        $this->allLanguages = [];
-        $this->allLanguagesUrls = [];
-        $this->dateSinceAMonth = date('Y-m-d', strtotime('-30 days'));
-        $this->apiUrl = "https://api.github.com/search/repositories?q=created:%3E$this->dateSinceAMonth&sort=stars&order=desc&per_page=1";
+    public function __construct() {
+        $this->languages = [];
+        $this->reposLanguagesUrls = [];
+        $this->dateSinceMonth = date('Y-m-d', strtotime('-30 days'));
+        $this->apiUrl = "https://api.github.com/search/repositories?q=created:%3E$this->dateSinceMonth&sort=stars&order=desc&per_page=5";
     }
 
-    public function getReposFromApi()
-    {
+    public function start() {
+        $this->storeLanguagesUrls($this->getReposFromApi());
+        $this->storeLanguages();
+    }
+
+    private function storeLanguagesUrls($repos) {
+        foreach ($repos as $repo) {
+            $this->reposLanguagesUrls[$repo['html_url']] = $repo['languages_url'];
+        }
+    }
+
+    private function getReposFromApi() {
         $response = Http::get($this->apiUrl);
-        return $response->json('items');
+        if ($response->ok()) {
+            return $response->json('items');
+        }
+    }
+
+    private function setAllLanguages($language) {
+        $this->languages[$language] = new LanguageController($language);
     }
 }
